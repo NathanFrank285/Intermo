dcd5294120c6e173cd253f75f1b9e9e0d8892e1c
NathanFrank285 <nathansfrank@yahoo.com>
2021-05-05 10:48:36 -0700

    delete route is complete

--- python-starter/app/api/post_routes.py
+++ python-starter/app/api/post_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import current_user, login_required
-from app.models import Currency, Post
+from app.models import db, Currency, Post
+from sqlalchemy import and_
 from forex_python.converter import CurrencyRates
 
 c = CurrencyRates()
@@ -15,22 +16,31 @@ def getPosts():
   posts = Post.query.filter(Post.userId == id).all()
   posts = [post.to_dict() for post in posts]
   print(posts)
-  return f'{id}'
+  return f'{posts}'
 
 
 @postRoutes.route('/', methods=['POST'])
 @login_required
-def getPosts():
+def postPosts():
+  # use the request object from flask to access the body of post request
+
   return
 
 
-@postRoutes.route('/', methods=['DELETE'])
+@postRoutes.route('/<postId>', methods=['DELETE'])
 @login_required
-def getPosts():
-  
+def deletePosts(postId):
+  id = current_user.id
+  post = Post.query.filter(and_(Post.userId == id, Post.id == postId)).first()
+  db.session.delete(post)
+  db.session.commit()
   return 'success'
 
-@postRoutes.route('/', methods=['PUT'])
+
+@postRoutes.route('/<postId>', methods=['PUT'])
 @login_required
-def getPosts():
+def putPosts(postId):
+  id = current_user.id
+  post = Post.query.filter(and_(Post.userId == id, Post.id == postId)).first().to_dict()
+
   return
