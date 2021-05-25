965027664cf22801cf1b4d68acbc5edbafc9ec7b
NathanFrank285 <nathansfrank@yahoo.com>
2021-05-25 11:08:07 -0700

    added notifications successful and failed trades

--- python-starter/react-app/src/components/Posts/index.js
+++ python-starter/react-app/src/components/Posts/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, NavLink } from "react-router-dom";
 import ExchangeRates from '../ExchangeRates'
 import {newTradeThunk} from '../../store/trades'
+import { store } from "react-notifications-component";
 // import { getUserBalanceThunk } from "../../store/userBalance";
 import "./Posts.css";
 
@@ -21,10 +22,12 @@ function Posts() {
 
 
   let numberOfPosts;
+
   if (posts) {
     numberOfPosts = Object.entries(posts).length
     posts = Object.entries(posts)
   }
+
 const submitTrade = async (date, postedCurrencyId, makerDirection, price, quantity, makerId, postId) => {
   const data = {
     date,
@@ -38,9 +41,42 @@ const submitTrade = async (date, postedCurrencyId, makerDirection, price, quanti
     baseName,
     quoteName
   };
-  await dispatch(newTradeThunk(data))
-  history.push('/')
-}
+
+  const {response} = await dispatch(newTradeThunk(data))
+  console.log(response, "-----------")
+
+  if (response === 'success') {
+      store.addNotification({
+        title: "Trade Complete!",
+        message: "Success, your trade is confirmed",
+        type: "success",
+        insert: "bottom",
+        container: "bottom-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+          onScreen: true,
+        }
+      });
+      history.push("/");
+    } else {
+      store.addNotification({
+        title: "Trade unsuccesful",
+        message: "Please check you have enough funds and try again",
+        type: "danger",
+        insert: "bottom",
+        container: "bottom-right",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+          onScreen: true,
+        }
+      })
+      history.push('/')
+
+}}
 // bidOrOffer: "offer";
 // created_on: "Wed, 05 May 2021 00:00:00 GMT";
 // id: 1;
