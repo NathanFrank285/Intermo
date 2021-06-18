d5e8456fc8e34c3e3ec3db82beac5b2239d1872a
NathanFrank285 <nathansfrank@yahoo.com>
2021-06-18 10:15:03 -0400

    updating the readme

--- Intermo/Front-end/src/components/AboutIntermo/index.js
+++ Intermo/Front-end/src/components/AboutIntermo/index.js
@@ -8,7 +8,7 @@ function AboutIntermo() {
       <div className="">Banks, on average, take a 1%-3% conversion fee just to swap from one currency to another. Intermo offers a market for individuals to avoid these fees by swapping their currencies with other users across the globe. Don't pay a middleman, use Intermo.</div>
 
       <h2>How to use Intermo?</h2>
-      <div></div>
+      <div>Start by using the search box to find currency pair you'd want to trade. make sure that if you are buying, you have the currency on the right of the slash. If you are selling, that you own the currency to the left of the slash.</div>
     </div>
 
   );
